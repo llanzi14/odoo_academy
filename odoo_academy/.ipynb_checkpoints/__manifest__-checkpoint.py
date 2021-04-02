4397a945f1421ce4bbbed8d7f18c8134b297444b
llanzi14 <lanziottilucas@gmail.com>
2021-04-02 13:25:26 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,33 @@
+# -*- coding: uft-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage training""",
+    
+    'description': """
+        Academy module to manage training:
+        - courses
+        - sessions
+        - attendees
+    """,
+    
+    'author': 'lzi',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+}
